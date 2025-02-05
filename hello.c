a1b767e358fe82041bde3bfa4c2213710fce5a09
Kiruthik-M <kiruthik05.m@gmail.com>
2025-02-05 16:19:47 +0530

    alpha

--- hello.c
+++ hello.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+
+
 int main() {
-    printf("Hello, World!\n");
+    char ch;
+
+    
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    
+    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+        printf("%c is an alphabet.\n", ch);
+    } else {
+        printf("%c is not an alphabet.\n", ch);
+    }
+
     return 0;
 }
